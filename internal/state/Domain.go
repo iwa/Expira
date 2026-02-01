5df1cfe48f53cdcd6c7fdbbdd31a1d7078ac1024
iwa <iwaongit@gmail.com>
2026-02-01 16:07:21 +0100

    feat: create GetDaysUntilExpiry method for Domain

--- internal/state/Domain.go
+++ internal/state/Domain.go
@@ -7,3 +7,18 @@ type Domain struct {
 	Exists     bool
 	ExpiryDate time.Time
 }
+
+// Calculate how many days remaining until Domain expiry date
+// Diff is calculated from time.Now()
+//
+// If the domain is already expired, returns -1
+func (d *Domain) GetDaysUntilExpiry() int {
+	currentTime := time.Now()
+	daysLeft := int(d.ExpiryDate.Sub(currentTime).Hours()/24) + 1 // Add 1 to include the current day
+
+	if daysLeft < 0 {
+		return -1
+	}
+
+	return daysLeft
+}
